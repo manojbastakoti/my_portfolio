9d07a19d4d64d7f54bd16cada5dd40f174a06840
Manoj Bastakoti <manojbastakoti252@gmail.com>
2023-10-05 10:05:38 +0545

    updating projects

--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -7,7 +7,8 @@ const projectData = [
   {
     id: 1,
     title: "My Portfolio",
-    description: "Project 1 Description",
+    description:
+      "Well,This is basically my portfolio site which I have created using Next.js and I have listed all my projects that I have worked on with the experience of technology on demand of the market.",
     image: "/image/projects/project1-img.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/manojbastakoti/my_portfolio",
@@ -16,19 +17,41 @@ const projectData = [
   {
     id: 2,
     title: "Ylite",
-    description: "Project 2 Description",
+    description:
+      "This is kind of youtube clone but I have implemented iframe links instead of api to load the youtube videos.I have used react as a frontend and node.js as backend for this.",
     image: "/image/projects/project2-img.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/manojbastakoti/Ylite",
     previewUrl: "/",
   },
   {
-    id: 1,
+    id: 3,
     title: "Toolkit Movie App",
-    description: "Project 3 Description",
+    description:
+      "This project is basically a learning phase of a toolkit with a movie clone app where people can search movies and shows they want to watch.",
     image: "/image/projects/project3-img.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/manojbastakoti/Toolkit-Movie-app",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "Bookshelf frontend App",
+    description:
+      "This project is a e-commerce site where I have used react.js as a frontend technology.It is full functional e-commerce site with features of recommendation of books based on user ratings,wishlist,cart,payment gateway,book listing,blog site  ",
+    image: "/image/projects/project4-img.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/manojbastakoti/Bookshelf_frontend",
+    previewUrl: "/",
+  },
+  {
+    id: 5,
+    title: "Bookshelf backend api",
+    description:
+      "All the api for the bookshelf site are done here with node.js",
+    image: "/image/projects/project5-img.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/manojbastakoti/Bookshelf_backend",
     previewUrl: "/",
   },
 ];
